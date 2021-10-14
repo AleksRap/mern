841f8770c4bb210ab7b08864f02e674a497b9d4b
Седов Алексей <aleks.sedov@mail.ru>
2021-10-14 22:22:16 +0300

    added model User

---
+++ models/User.ts
@@ -0,0 +1,19 @@
+import { Schema, model, Types } from 'mongoose';
+
+const schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  links: [{
+    type: Types.ObjectId,
+    ref: 'Link',
+  }],
+});
+
+export const User = model('User', schema);
