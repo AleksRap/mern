ae9b11aa5ff2731d6222c58c34809c2c2e62cb36
Седов Алексей <aleks.sedov@mail.ru>
2021-10-14 22:21:28 +0300

    added routes

---
+++ routes/auth.routes.ts
@@ -0,0 +1,70 @@
+import { Router, Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import { check, validationResult } from 'express-validator';
+import { User } from '@models';
+import { Validations } from '@constants';
+
+export const router = Router();
+
+// /api/auth/register
+router.post(
+  '/register',
+  [
+    check('email', 'Некорректный email').isEmail(),
+    check('password', `Минимальная длина пароля ${Validations.passMinLength} символов`)
+      .isLength({ min: Validations.passMinLength }),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректные данные при регистрации',
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: 'Такой пользователь уже существует' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: 'Пользователь создан' });
+
+    } catch (e) {
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+    }
+  });
+
+// /api/auth/login
+router.post(
+  '/login',
+  [
+    check(),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректные данные при входе в систему',
+        });
+      }
+
+
+
+    } catch (e) {
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+    }
+  });
